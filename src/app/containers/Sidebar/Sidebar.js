05d6fc8fe1f987f8849fed504bd27b489da60bda
Andrix <>
2019-09-25 16:13:09 +0700

    Load menuitems from config file

--- src/app/containers/Sidebar/Sidebar.js
+++ src/app/containers/Sidebar/Sidebar.js
@@ -6,9 +6,9 @@ import SidebarWrapper from './sidebar.style';
 
 import Logo from './Logo';
 import {siteConfig} from '../../data/index';
+import menus from "../../data/menu";
 
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
+
 const {Sider} = Layout;
 
 
@@ -42,56 +42,39 @@ const Sidebar = ({
                 // style={styling}
             >
                 <Logo collapsed={collapsed}/>
-                    <Menu
-                        // onClick={this.handleClick}
-                        theme="dark"
-                        mode={mode}
-                        // openKeys={collapsed ? [] : app.openKeys}
-                        // selectedKeys={app.current}
-                        // onOpenChange={this.onOpenChange}
-                        className="isoDashboardMenu"
-                    >
-                        <Menu.Item key="mailbox">
-                            <Link href={`${url}`}>
-                                <a className="isoMenuHolder" style={submenuColor}>
-                                    <i className="ion-android-mail"/>
-                                    <span className="nav-text">
-                      Text1
-                    </span>
-                                </a>
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item key="chat">
-                            <Link href={`${url}/page2`}>
-                                <a className="isoMenuHolder" style={submenuColor}>
-                                    <i className="ion-chatbubbles"/>
-                                    <span className="nav-text">
-                      Text2
-                    </span>
-                                </a>
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item key="invoice">
-                            <Link href={`${url}/invoice`}>
-                                <a className="isoMenuHolder" style={submenuColor}>
-                                    <i className="ion-clipboard"/>
-                                    <span className="nav-text">
-                      Text3
-                    </span>
-                                </a>
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item key="contacts">
-                            <Link href={`${url}/contacts`}>
-                                <a className="isoMenuHolder" style={submenuColor}>
-                                    <i className="ion-android-person-add"/>
-                                    <span className="nav-text">
-                      Text4
-                    </span>
-                                </a>
-                            </Link>
-                        </Menu.Item>
-                    </Menu>
+                <Menu
+                    // onClick={this.handleClick}
+                    theme="dark"
+                    mode={mode}
+                    // openKeys={collapsed ? [] : app.openKeys}
+                    // selectedKeys={app.current}
+                    // onOpenChange={this.onOpenChange}
+                    className="isoDashboardMenu"
+                >
+                    {menus.map( (menu, key) => {
+                        return (
+                            <Menu.ItemGroup
+                              key={key}
+                              title={menu.group}
+                            >
+                                {menu.items.map( (item, idx)=>{
+                                    return (
+                                        <Menu.Item key={`${key}${idx}`}>
+                                            <Link href={item.path}>
+                                                <a className="isoMenuHolder" style={submenuColor}>
+                                                    <i className={item.icon}/>
+                                                    <span className="nav-text">
+                                                        {item.name}
+                                                    </span>
+                                                </a>
+                                            </Link>
+                                        </Menu.Item>
+                                    );
+                                })}
+                            </Menu.ItemGroup>
+                        )
+                    } )}
+                </Menu>
             </Sider>
         </SidebarWrapper>
     );
